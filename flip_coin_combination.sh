c70a955a3e407ac2735ecf3935345f8c603abf64
anupama cv <anu.anupamacv@gmail.com>
2021-05-15 16:57:56 +0530

    added singlet combination

--- flip_coin_combination.sh
+++ flip_coin_combination.sh
@@ -14,4 +14,26 @@ dictionary[$i]="TAIL"
 fi
 done
 echo ${dictionary[@]}
+count_of_head=0
+count_of_tail=0
+for((i=1;i<=num;i++))
+do
+if [ "${dictionary[i]}" = "HEAD" ]
+then
+(( count_of_head++ ))
+elif [ "${dictionary[i]}" = "TAIL" ]
+then
+(( count_of_tail++ ))
+fi
+done
+echo "Number of HEAD is = "$count_of_head 
+echo "Number of TAIL is = "$count_of_tail
+if [ $count_of_head -gt $count_of_tail ]
+then
+percentage1=$(( $count_of_head*100/$num ))
+echo "HEAD is the winner and winning percentage is = "$percentage1 "%"
+else
+percentage2=$(( $count_of_tail*100/$num ))
+echo "TAIL is the winner and winning percentage is = "$percentage2 "%"
+fi
 esac
