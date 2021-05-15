79fd744e549a7f974e454d3b6dfd6aec7adc4fc6
anupama cv <anu.anupamacv@gmail.com>
2021-05-15 12:30:20 +0530

    added singlet combination

--- flip_coin_combination.sh
+++ flip_coin_combination.sh
@@ -1,1 +1,16 @@
+read -p "Enter the number of times you want to flip the coin " num 
+read -p "Enter your choice " choice
 
+case $choice in
+
+1)  for((i=1;i<=num;i++))
+do
+coin=$(($RANDOM%2))
+if [ $coin -eq 1 ] 
+then 
+dictionary[$i]="HEAD"
+else
+dictionary[$i]="TAIL"
+fi
+done
+esac
