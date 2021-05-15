c5c416393408ca6c2edca5e23775da41284b29c0
anupama cv <anu.anupamacv@gmail.com>
2021-05-15 17:08:18 +0530

    added program for flipping a coin

--- flip_coin_combination.sh
+++ flip_coin_combination.sh
@@ -1,1 +1,12 @@
+read -p "Enter the number of times you want to flip the coin " num 
+for((i=1;i<=num;i++))
+do
+coin=$(($RANDOM%2))
+if [ $coin -eq 1 ] 
+then 
+echo "HEAD"
+else
+echo "TAIL"
+fi
+done
 
